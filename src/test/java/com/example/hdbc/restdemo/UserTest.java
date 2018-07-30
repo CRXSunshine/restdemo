a79829b19a2f8216f3955d0717147d43c950c158
crx笑看风云动 <2235390423@qq.com>
2018-07-30 18:00:03 +0800

    init project

--- src/test/java/com/example/hdbc/restdemo/UserTest.java
+++ src/test/java/com/example/hdbc/restdemo/UserTest.java
@@ -1,13 +1,30 @@
 package com.example.hdbc.restdemo;
 
+import com.alibaba.fastjson.JSON;
 import com.example.hdbc.restdemo.domain.User;
 import com.example.hdbc.restdemo.service.UserService;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static sun.nio.cs.Surrogate.is;
 
 /**
  * @author:Kevin
@@ -21,15 +38,88 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 public class UserTest {
     @Autowired
+    private WebApplicationContext context;
     private MockMvc mockMvc;
-    private UserService userService=new UserService();
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();  //构造MockMvc
+
+    }
+
     @Test
     public void saveUser(){
         User user=new User();
         user.setId("20150611");
-        user.setUsername("kevin");
+        user.setUsername("头号玩家");
         user.setPassword("123456");
-        User user1=userService.saveUser(user);
-        System.out.println(user1);
+        String json=JSON.toJSONString(user);
+        try {
+            mockMvc.perform(post("/user")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .param(json)
+                    .accept(MediaType.APPLICATION_JSON))  //接收的类型
+                    .andExpect(status().isOk())   //判断接收到的状态是否是200
+                    .andDo(print())  //打印内容
+                    .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect((ResultMatcher) content().string(Matchers.containsString("OK"))); //匹配返回值中的内容
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(json);
+    }
+
+    @Test
+    public void updateUser(){
+        User user=new User();
+        user.setId("20150611");
+        user.setUsername("奇点");
+        user.setPassword("123456");
+        String json=JSON.toJSONString(user);
+        try {
+            mockMvc.perform(put("/user")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .param(json)
+                    .accept(MediaType.APPLICATION_JSON))  //接收的类型
+                    .andExpect(status().isOk())   //判断接收到的状态是否是200
+                    .andDo(print())  //打印内容
+                    .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect((ResultMatcher) content().string(Matchers.containsString("OK"))); //匹配返回值中的内容
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(json);
+    }
+
+    @Test
+    public void queryByUserId() {
+        String id="20150611";
+        try {
+            mockMvc.perform(put("/user")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .param(id)
+                    .accept(MediaType.APPLICATION_JSON))  //接收的类型
+                    .andExpect(status().isOk())   //判断接收到的状态是否是200
+                    .andDo(print())  //打印内容
+                    .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect((ResultMatcher) content().string(Matchers.containsString("OK"))); //匹配返回值中的内容
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void deleteByUserId() {
+        String id="20150611";
+        try {
+            mockMvc.perform(delete("/user")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .param(id)
+                    .accept(MediaType.APPLICATION_JSON))  //接收的类型
+                    .andExpect(status().isOk())   //判断接收到的状态是否是200
+                    .andDo(print())  //打印内容
+                    .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect((ResultMatcher) content().string(Matchers.containsString("OK"))); //匹配返回值中的内容
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
