0438c71a8a099a890e601747e52fd5d15032e0e2
crx笑看风云动 <2235390423@qq.com>
2018-08-01 10:03:58 +0800

    update User

--- src/main/java/com/example/hdbc/restdemo/domain/User.java
+++ src/main/java/com/example/hdbc/restdemo/domain/User.java
@@ -1,5 +1,7 @@
 package com.example.hdbc.restdemo.domain;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -12,33 +14,11 @@ import javax.persistence.Id;
  * @Date date: 2018/7/30
  */
 @Entity
+@Data
 public class User {
     @Id
     private String id;
     private String username;
     private String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/test/java/com/example/hdbc/restdemo/UserTest.java
+++ src/test/java/com/example/hdbc/restdemo/UserTest.java
@@ -2,29 +2,19 @@ package com.example.hdbc.restdemo;
 
 import com.alibaba.fastjson.JSON;
 import com.example.hdbc.restdemo.domain.User;
-import com.example.hdbc.restdemo.service.UserService;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static sun.nio.cs.Surrogate.is;
 
 /**
  * @author:Kevin
